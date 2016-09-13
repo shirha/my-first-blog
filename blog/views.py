eafaec24d6e91e0d592b344bc88c5408e4a5af20
shirha <sh1cpan@gmail.com>
2016-09-12 22:04:50 -0700

    Added views to create/edit blog post inside the site.

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,3 @@
-from django.shortcuts import render
-
-# Create your views here.
-
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
